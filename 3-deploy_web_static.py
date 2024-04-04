8c4e1dfdba2ef297d02c6659d83726654cd72b5b
mounimnadir7@gmail.com <mounimnadir7@gmail.com>
2024-04-03 19:51:00 -0700

    new tasks updated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,53 +6,47 @@ distributes an archive to the web servers
 
 from fabric.api import env, local, put, run
 from datetime import datetime
-from os.path import basename, isdir
-from fabric.exceptions import NetworkError
-
+from os.path import exists, isdir
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
+
 def do_pack():
-    """Generates a tgz archive"""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if not isdir("versions"):
-            local("mkdir -p versions")
+        if isdir("versions") is False:
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
-        print("Error occurred during packaging:", e)
+    except:
         return None
 
+
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
     try:
-        if not archive_path or not basename(archive_path).endswith('.tgz'):
-            return False
-        file_n = basename(archive_path)
+        file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
-        remote_path = "/data/web_static/releases/{}/".format(no_ext)
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(remote_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_n, remote_path))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {}{}/web_static/* {}{}/'.format(remote_path, no_ext, remote_path, no_ext))
-        run('rm -rf {}{}/web_static'.format(remote_path, no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(remote_path))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except NetworkError as ne:
-        print("Network error occurred:", ne)
+    except:
         return False
-    except Exception as e:
-        print("Error occurred during deployment:", e)
-        return False
+
 
 def deploy():
-    """Creates and distributes an archive to the web servers"""
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
-    if archive_path:
-        return do_deploy(archive_path)
-    else:
+    if archive_path is None:
         return False
-
+    return do_deploy(archive_path)
